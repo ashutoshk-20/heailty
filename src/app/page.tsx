94994c7a4cda35a9a9a88daa248b8ec98164794a
Ashutosh Khedkar <ashutoshkhedkar04.com>
2025-08-13 10:57:10 +0530

    database and authentication setup completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+"use client";
+import { SignedIn, SignedOut, SignInButton } from '@clerk/clerk-react'
+import { SignOutButton } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>page</div>
+    <div>
+      HomePage
+      <SignedOut>
+        <SignInButton/>
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton/>
+      </SignedIn>
+    </div>
   )
 }
 
